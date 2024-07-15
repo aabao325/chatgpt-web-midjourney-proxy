03b7dd2a2ca69ef35bd396faf39e2087b4eb5a48
aabao325 <62531785+aabao325@users.noreply.github.com>
2024-07-15 21:07:08 +0800

    去掉作者信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
-      name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: '',
+      name: '点此设置名字',
+      description: '<a href="http://web.aabao.vip" class="text-blue-500" target="_blank" >Access to OpenAI</a>',
     },
   }
 }
